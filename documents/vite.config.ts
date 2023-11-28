5e5cde48e6108484ff715ebe91868ee13e530b2d
Ekaterina Popova <kartyme0@gmail.com>
2023-11-29 00:36:09 +0300

    pages

--- documents/vite.config.ts
+++ documents/vite.config.ts
@@ -3,13 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  
   server: {
     proxy: {
       '/documents': 'http://127.0.0.1:8000/'
     },
     port:3000
   },
+  base: "/",
   
   plugins: [react()],
 })
